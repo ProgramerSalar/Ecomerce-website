c8cf069722b3891721e44d7d31c0b61dc0452192
manish <manishkumar60708090@gmail.com>
2024-01-01 20:54:40 +0530

    product card

--- Frontent/src/pages/Home.tsx
+++ Frontent/src/pages/Home.tsx
@@ -1,11 +1,34 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-cart";
 
 const Home = () => {
+  const addToCartHandler = () => {};
   return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+    <div className="home">
+      <section></section>
 
-export default Home
+
+
+<h1>Latest Product</h1>
+      <Link to="/search" className="findmore">
+        More
+      </Link>
+
+    
+     
+
+      <main>
+        <ProductCard
+          productId="daoiere"
+          name="macbook"
+          price={23}
+          stock={23}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71jG+e7roXL._AC_UY218_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
